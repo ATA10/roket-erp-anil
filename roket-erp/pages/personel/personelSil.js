5291b4469e819ebed58f840c5f3ed0258f8b09fa
aniltaha <60629661+ATA10@users.noreply.github.com>
2022-06-03 23:21:36 +0300

    Delete roket-erp directory

--- roket-erp/pages/personel/personelSil.js
+++
@@ -1,8 +0,0 @@
-var baglanti = require('../../baglanti');
-function personelSil() {
-    var secilmisKisiler = document.querySelectorAll('input[type="checkbox"]:checked')
-    for (i = 0; i < secilmisKisiler.length; i++) {
-        baglanti.connection.query("DELETE FROM `personel` WHERE `id`= " + secilmisKisiler[i].value)
-    }
-    location.reload();
-}
