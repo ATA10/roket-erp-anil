354fb456249332d76639db0b7ffc53c2387273d1
aniltaha <60629661+ATA10@users.noreply.github.com>
2022-06-09 12:46:17 +0300

    Add files via upload

---
+++ erp/baglanti.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+const alert=require('alert');
+
+const host = "localhost";
+const port = 3306;
+const user = "root";
+const password = "";
+const database = "roketerp";
+
+let connection = mysql.createConnection({
+    host,
+    user,
+    password,
+    port,
+    database
+});  
+
+connection.connect(function(err) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("Connected!");
+    }
+});
+
+
+module.exports = { connection };
